c86aa01715b0c5d0fa1323ec215fe3d4efc2518f
puroc <p00u00r00>
2016-11-13 22:18:08 +0800

    Initial commit

---
+++ src/main/java/com/example/data/analysis/DataAnalysisEngineApplication.java
@@ -0,0 +1,12 @@
+package com.example.data.analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAnalysisEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataAnalysisEngineApplication.class, args);
+	}
+}
---
+++ src/main/resources/static/app/js/app.js
@@ -0,0 +1,12 @@
+var dae = angular.module('dae', ['ngRoute']);
+
+dae.config(['$routeProvider',function($routeProvider) {
+	$routeProvider.when('/flow', {
+		templateUrl:'tpls/flow.html'
+	}).when('/interpreter', {
+		templateUrl:'tpls/interpreter.html'
+	}).otherwise({
+		redirectTo: '/interpreter'
+	})
+}]);
+
